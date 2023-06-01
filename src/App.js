9296ecf9753b7de871761b9e73fe6b7352a646ba
Joanna Makarewicz <joanna.milun@gmail.com>
2023-06-01 11:32:38 +0100

    LoadingIcon component added & variables changes in App component

--- src/App.js
+++ src/App.js
@@ -14,51 +14,47 @@ import Summary from "./pages/Summary/Summary";
 import Register from "./pages/Register/Register";
 import axiosInstance from "./services/config";
 
-
 function App() {
-
   const [pictures, setPictures] = useState([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  const content = (
+    <Home>
+      <Header />
+      <Content pictures={pictures}/>
+      <Footer />
+    </Home>
+  )
 
   const getPicture = async () => {
     await axiosInstance.get("/pictures").then((response) => {
       setPictures(response.data.records);
     });
   };
-  
+
   useEffect(() => {
     getPicture();
   }, []);
 
- 
   return (
-    <AuthContext.Provider value = {{
-      isAuthenticated: isAuthenticated,
-      login: () => setIsAuthenticated(true),
-      logout: () => setIsAuthenticated(false)
-      }}>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated: isAuthenticated,
+        login: () => setIsAuthenticated(true),
+        logout: () => setIsAuthenticated(false),
+      }}
+    >
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Home>
-                <Header />
-                <Content pictures={pictures}/>
-                <Footer />
-              </Home>
-            }
-          />
+          <Route path="/" element={content} />
           <Route path="/zaloguj" element={<Login />} />
           <Route path="/zarejestruj" element={<Register />} />
-          <Route path="/wyszukaj/:term" element={<Search pictures={pictures}/>} />
-          <Route path="/koszyk" element={<Summary pictures={pictures}/>} />
+          <Route path="/wyszukaj/:term" element={<Search pictures={pictures} />}/>
+          <Route path="/koszyk" element={<Summary pictures={pictures} />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
     </AuthContext.Provider>
-
   );
 }
 
