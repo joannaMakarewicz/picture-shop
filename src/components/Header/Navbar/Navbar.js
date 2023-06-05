e1066e56300669f4b5573aad6a3a1936f7c6d7c9
Joanna Makarewicz <joanna.milun@gmail.com>
2023-06-05 19:05:15 +0100

    Bug with hamburger icon solved

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import { RxHamburgerMenu } from 'react-icons/rx';
-import { RxCross1 } from 'react-icons/rx';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross1 } from "react-icons/rx";
 import useAuth from "../../../hooks/useAuth";
 import "../Navbar/Navbar.css";
 
 const Navbar = () => {
-
   const [auth, setAuth] = useAuth();
   const [inputValue, setInputValue] = useState("");
-  const [active, setActive]= useState(false);
+  const [active, setActive] = useState(false);
   const inputRef = useRef();
   const listOfItems = useNavigate();
 
@@ -31,92 +30,96 @@ const Navbar = () => {
     focusInput();
   }, []);
 
-
   const logout = (e) => {
     e.preventDefault();
     setAuth(false);
   };
 
   const showMenu = () => {
-    setActive(!active)
+    setActive(!active);
   };
 
   return (
     <nav className="mainNavbar navbar justify-content-between align-items-center">
-        <button className="mainNavbar__hamburger" onClick={showMenu}>
-        {active
-  ?      
-          <RxCross1 className="w-100 h-100"/>
-  :
-          <RxHamburgerMenu className="w-100 h-100"/>
+      <button className="mainNavbar__hamburger" onClick={showMenu}>
+        {active ? (
+          <RxCross1 className="w-100 h-100" />
+        ) : (
+          <RxHamburgerMenu className="w-100 h-100" />
+        )}
+      </button>
+      <ul
+        className={
+          active
+            ? "mainNavbar__navigation mainNavbar__navigation--open text-center m-0 p-0"
+            : "mainNavbar__navigation text-center m-0 p-0"
         }
-        </button>
-        <ul className={active?  "mainNavbar__navigation mainNavbar__navigation--open text-center m-0 p-0" : "mainNavbar__navigation text-center m-0 p-0"}>
-          <li>
-            <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")} to="/">
-              Home
-            </NavLink>
-          </li>
-
-          {auth ? (
-            <>
-              <li className="mainNavbar__link">
-                <NavLink
-                  className={({ isActive }) => (isActive ? "active" : "inactive")}
-                  to="/bag"
-                >
-                  My cart
-                </NavLink>
-              </li>
-              <li className="mainNavbar__link">
-                <a
-                  className={({ isActive }) => (isActive ? "active" : "inactive")}
-                  href="/login"
-                  onClick={logout}
-                >
-                  Log out
-                </a>
-              </li>
-            </>
-          ) : (
-            <>
-              <li className="mainNavbar__link">
-                <NavLink
-                  className={({ isActive }) => (isActive ? "active" : "inactive")}
-                  to="/login"
-                >
-                  Log in
-                </NavLink>
-              </li>
-              <li className="mainNavbar__link">
-                <NavLink
-                  className={({ isActive }) => (isActive ? "active" : "inactive")}
-                  to="/register"
-                >
-                  Register
-                </NavLink>
-              </li>
-            </>
-          )}
-        </ul>
-        <div className=" searchbar d-flex flex-row align-items-center">
-          <input
-            className="searchbar__input me-2 h-100"
-            ref={inputRef}
-            type="text"
-            placeholder="Search"
-            aria-label="Search"
-            onKeyDown={onKeyDownHandler}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <button
-            className="btn btn-outline-success h-100"
-            onClick={inputSearch}
+      >
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="/"
           >
-            Search
-          </button>
-        </div>
-      </nav>
+            Home
+          </NavLink>
+        </li>
+
+        {auth ? (
+          <>
+            <li className="mainNavbar__link">
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : "inactive")}
+                to="/bag"
+              >
+                My cart
+              </NavLink>
+            </li>
+            <li className="mainNavbar__link">
+              <a
+                className={({ isActive }) => (isActive ? "active" : "inactive")}
+                href="/login"
+                onClick={logout}
+              >
+                Log out
+              </a>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="mainNavbar__link">
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : "inactive")}
+                to="/login"
+              >
+                Log in
+              </NavLink>
+            </li>
+            <li className="mainNavbar__link">
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : "inactive")}
+                to="/register"
+              >
+                Register
+              </NavLink>
+            </li>
+          </>
+        )}
+      </ul>
+      <div className=" searchbar d-flex flex-row align-items-center">
+        <input
+          className="searchbar__input me-2 h-100"
+          ref={inputRef}
+          type="text"
+          placeholder="Search"
+          aria-label="Search"
+          onKeyDown={onKeyDownHandler}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button className="btn btn-outline-success h-100" onClick={inputSearch}>
+          Search
+        </button>
+      </div>
+    </nav>
   );
 };
 
