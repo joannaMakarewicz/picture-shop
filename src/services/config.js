3f29d5d9b8c76b76eef8235d9a2ddcf0e618e71c
Joanna Makarewicz <joanna.milun@gmail.com>
2023-10-16 09:08:16 +0100

    changing way of auth in airtable

--- src/services/config.js
+++ src/services/config.js
@@ -2,6 +2,7 @@ import axios  from "axios";
 
 const { REACT_APP_DB_ID, REACT_APP_API_KEY } = process.env
 
+
 const BASE_URL = `https://api.airtable.com/v0/${REACT_APP_DB_ID}`;
 
 const axiosInstance = axios.create({
