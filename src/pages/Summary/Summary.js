f30985478f871cbab5823b27a09928df0607dfb5
Joanna Makarewicz <joanna.milun@gmail.com>
2023-10-17 13:28:38 +0100

    My Card page changes - info after buing & info when bag is empty

--- src/pages/Summary/Summary.js
+++ src/pages/Summary/Summary.js
@@ -6,10 +6,18 @@ import Order from "../../components/Order/Order";
 import MakeOrder from "../../components/MakeOrder/MakeOrder";
 import Footer from "../../components/Footer/Footer";
 import Button from "../../components/Button/Button";
+import { useNavigate } from "react-router-dom";
 import "../Summary/Summary.scss";
 
 const Summary = ({ pictures }) => {
   useWebsiteTitle("Order summary");
+  const navigate = useNavigate();
+
+  const historyOfBuying = pictures.map((picture) => {
+    return JSON.parse(localStorage.getItem(`purchase: ${picture.id}`));
+  });
+
+  const isSthBought = historyOfBuying.filter((item) => item === true);
 
   const [infoAfterOrder, setInfoAfterOrder] = useState(false);
 
@@ -23,26 +31,32 @@ const Summary = ({ pictures }) => {
     setInfoAfterOrder(true);
   };
 
+  const goHome = () => {
+    navigate("/")
+  }
+
   return (
     <div className="summary container-lg text-light">
       <Navbar />
-      <h1 className="summary__heading mt-3 mb-3">Your card:</h1>
-      <main className="summary__content">
-        {infoAfterOrder ? (
-          <>
-            <MakeOrder />
-            <TotalCost pictures={pictures} />
-          </>
-        ) : (
-          <>
-            <Order pictures={pictures} infoAfterOrder={infoAfterOrder} />
-            <TotalCost pictures={pictures} />
-            <Button onClick={makeOrder} />
-          </>
-        )}
-      </main>
+      {isSthBought.length>0 ? (
+        <>
+          <h1 className="summary__heading mt-3 mb-3">Your card:</h1>
+          <main className="summary__content">
+          <Order pictures={pictures}/>
+          <TotalCost pictures={pictures} />
+          <Button pictures={pictures} onClick={makeOrder}/>
+          </main>
+        </>
+      ) : (
+        <>
+        {infoAfterOrder?<MakeOrder /> : null}
+        <TotalCost pictures={pictures} />
+        <button className="btn btn-success" onClick={goHome}>Find sth</button>
+        </>
+      )}
     </div>
   );
 };
 
 export default Summary;
+
